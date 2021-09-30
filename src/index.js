22ce270527cc2979b33fc41b017ecf75a92d79be
isaka-lumato <75973193+isaka-lumato@users.noreply.github.com>
2021-09-30 18:21:11 +0300

    changed file path from absolute to relative

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom"; 
-import TodoContainer from "/home/isaka/to-do/src/functionalBased/components/TodoContainer.js";
-import "/home/isaka/to-do/src/functionalBased/App.css";
+import TodoContainer from "./functionalBased/components/TodoContainer";
+import "./functionalBased/App.css";
 
 ReactDOM.render(
   <React.StrictMode>
