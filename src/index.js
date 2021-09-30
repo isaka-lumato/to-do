d03988ca6c5301ff8851698c30034feb70dbbe3d
isaka-lumato <75973193+isaka-lumato@users.noreply.github.com>
2021-09-30 13:24:41 +0300

    github-pages

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 /* eslint-disable */
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter  } from "react-router-dom";
 import TodoContainer from "/home/isaka/to-do/src/functionalBased/components/TodoContainer.js";
 import "/home/isaka/to-do/src/functionalBased/App.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <TodoContainer />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
